4ddd69350c4e3324d867db31e929c4d05b1cf0c6
Gareth Porter <garethwporter@gmail.com>
2020-05-03 22:27:37 +0300

    Revert "add rest framework"
    This reverts commit 09de09fc

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products.apps.ProductsConfig',
-    'rest_framework',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
